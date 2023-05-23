3a7bfd45787ee6252714001520eff373c7388299
Arda Akcagöz <arda.akcagoz@outlook.com>
2023-05-23 15:22:17 +0300

    Checkbox finished.

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import "./Checkbox.css";
 
 interface CheckboxProps {
@@ -6,13 +5,15 @@ interface CheckboxProps {
 }
 
 export default function Checkbox(props: CheckboxProps) {
-    const [isChecked, setIsChecked] = useState(false);
     return (
-        <div className="checkbox-container">
-            <label>
-                <input type="checkbox" className={isChecked ? "checked": ""} checked={isChecked} onChange={() => setIsChecked((prev) => !prev)}/>
-                <span>{props.label}</span>
-            </label>
+        <div style={{ display:"inline-flex", flexDirection:"row" }}>
+            <div className="checkbox-container">
+                <div className="round">
+                    <input type="checkbox" id="checkbox-18" />
+                    <label htmlFor="checkbox-18"></label>
+                </div>
+            </div>
+            {props.label}
         </div>
     );
 }
