4a0c6d0b2a304376da9cba4541dd186694736bdc
Arda Akcagöz <arda.akcagoz@outlook.com>
2023-05-23 21:36:44 +0300

    Checkbox changes

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -2,18 +2,19 @@ import "./Checkbox.css";
 
 interface CheckboxProps {
     label: string,
+    description?: string,
 }
 
 export default function Checkbox(props: CheckboxProps) {
     return (
-        <div style={{ display:"inline-flex", flexDirection:"row" }}>
+        <div style={{ display:"inline-flex", flexDirection: "row", justifyContent: "center", }}>
             <div className="checkbox-container">
                 <div className="round">
                     <input type="checkbox" id="checkbox-18" />
                     <label htmlFor="checkbox-18"></label>
                 </div>
             </div>
-            {props.label}
+            <span style={{ padding: 0, fontWeight:"bold", }}>{props.label}</span>
         </div>
     );
 }
